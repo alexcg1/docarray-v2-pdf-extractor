73834828ad26e4d58f5b0af69f69d641a1364e0b
Alex C-G <alexcg@outlook.com>
2023-03-29 13:55:50 +0200

    feat: extract tables
    Signed-off-by: Alex C-G <alexcg@outlook.com>

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import csv
+import io
 import tempfile
+from pprint import pprint
 
 import pdfplumber
 from docarray import BaseDoc, DocArray
@@ -15,16 +18,25 @@ class PDFDocument(BaseDoc):
     mod_date: str | None
 
 
+class ImageChunk(ImageDoc):
+    tags: dict = {}
+
+
+class TextChunk(TextDoc):
+    tags: dict = {}
+
+
 class PDFExtractor:
     def add_chunks(docs: DocArray):
         # None of these map_docs lines do anything to the doc
-        map_docs(docs, PDFExtractor._extract_text)
-        map_docs(docs, PDFExtractor._extract_metadata, backend='thread')
-        map_docs(docs, PDFExtractor._extract_images)
+        # map_docs(docs, PDFExtractor._extract_text)
+        # map_docs(docs, PDFExtractor._extract_metadata, backend='thread')
+        # map_docs(docs, PDFExtractor._extract_images)
 
         # This works tho
         for doc in docs:
             PDFExtractor._extract_text(doc)
+            PDFExtractor._extract_tables(doc)
             PDFExtractor._extract_images(doc)
             PDFExtractor._extract_metadata(doc)
 
@@ -42,17 +54,49 @@ class PDFExtractor:
     def _extract_text(doc: PDFDocument):
         with pdfplumber.open(doc.path) as pdf:
             for page in pdf.pages:
-                for page in pdf.pages:
+                for page_no, page in enumerate(pdf.pages):
                     text = page.extract_text()
-                    text_doc = TextDoc(text=text)
+                    text_doc = TextDoc(
+                        text=text,
+                        tags={
+                            'media_type': 'image',
+                            'filename': 'doc.path',
+                            'page_no': page_no,
+                        },
+                    )
                     doc.chunks.append(text_doc)
 
-        return doc
+    def _extract_tables(doc: PDFDocument):
+        with pdfplumber.open(doc.path) as pdf:
+            for page in pdf.pages:
+                for page_no, page in enumerate(pdf.pages):
+                    tables = page.extract_tables()
+                    for table in tables:
+                        # pprint(table)
+                        output = io.StringIO()
+                        writer = csv.writer(
+                            output, quoting=csv.QUOTE_NONNUMERIC
+                        )
+                        writer.writerows(table)
+                        csv_data = output.getvalue()
+                        csv_doc = TextDoc(
+                            text=csv_data,
+                            tags={
+                                'media_type': 'table',
+                                'filename': 'doc.path',
+                                'page_no': page_no,
+                            },
+                        )
+                        doc.chunks.append(csv_doc)
+                        # Do something with the CSV data, e.g. print it
+                        # print(csv_data)
+                    # text_doc = TextDoc(text=text)
+                    # doc.chunks.append(text_doc)
 
     def _extract_images(doc: PDFDocument):
         with pdfplumber.open(doc.path) as pdf:
-            for p, page in enumerate(pdf.pages):
-                for i, image in enumerate(page.images):
+            for page_no, page in enumerate(pdf.pages):
+                for image in page.images:
                     image_bbox = (
                         image['x0'],
                         page.height - image['y1'],
@@ -75,6 +119,11 @@ class PDFExtractor:
                         url=output_path,
                         tensor=None,
                         embedding=None,
+                        tags={
+                            'media_type': 'image',
+                            'filename': 'doc.path',
+                            'page_no': page_no,
+                        },
                     )
                     image_doc.tensor = image_doc.url.load()
                     doc.chunks.append(image_doc)
