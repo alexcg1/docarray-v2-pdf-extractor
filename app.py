36cddd1c51105fdf641ab55bc489f452e7fe75cf
Alex C-G <alexcg@outlook.com>
2023-04-03 11:21:15 +0200

    feat: encode text chunks
    Signed-off-by: Alex C-G <alexcg@outlook.com>

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from docarray import DocArray
-from jina import Deployment
+from jina import Deployment, Flow
 
-from executor import PDFDocument, PDFExtractorExecutor
+from executor import CLIPEncoder, PDFDocument, PDFExtractorExecutor
 
 docs = DocArray[PDFDocument]([PDFDocument(path='rabbit.pdf')])
 
 content_types = ['text', 'image', 'table', 'metadata']
+content_types = ['text']
 
-dep = Deployment(
-    uses=PDFExtractorExecutor,
-    uses_with={'content_types': content_types},
+flow = (
+    Flow()
+    .add(
+        uses=PDFExtractorExecutor,
+        uses_with={'content_types': content_types},
+        name='extractor',
+    )
+    .add(uses=CLIPEncoder, name='encoder')
 )
 
-with dep:
-    output = dep.post(
+# dep = Deployment(
+# uses=PDFExtractorExecutor,
+# uses_with={'content_types': content_types},
+# )
+
+with flow:
+    output = flow.post(
         inputs=docs, on='/extract', return_type=DocArray[PDFDocument]
     )
 
